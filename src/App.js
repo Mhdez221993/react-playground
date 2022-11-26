500dc7e832acc8aade79d3ef53c37503f755acf0
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-26 19:55:51 +0100

    Conditionally run effects on class components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import './components/appStyle.css';
 
-import HookCounterOne from './components/HookCounterOne';
+import ClassCounterOne from './components/ClassCounterOne';
 import React from 'react';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <HookCounterOne />
+        {/* <HookCounterOne /> */}
+        <ClassCounterOne />
       </div>
     );
   }
