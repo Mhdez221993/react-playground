bd281cfb26743f19bd5782fae44e2f7b02269037
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-26 09:21:53 +0100

    Basics of working with forms

--- src/App.js
+++ src/App.js
@@ -1,46 +1,47 @@
 import './App.css';
 import './components/appStyle.css';
 
-import Inline from './components/Inline';
+import Form from './components/Form';
 import React from 'react';
-import style from './components/appStyle.module.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-      <h1 className="error">Error</h1>
-      <h1 className={style.success}>Success</h1>
+        <Form />
 
-        <Inline />
+        {/* <h1 className="error">Error</h1>
+        <h1 className={style.success}>Success</h1> */}
+
+        {/* <Inline /> */}
 
         {/* <Hello /> */}
 
-       {/* <Greet name="Greeting componenet" >
+        {/* <Greet name="Greeting componenet" >
+            <p>Children props</p>
+        </Greet> */}
+
+        {/* <Welcome name="Welcome component">
           <p>Children props</p>
-       </Greet> */}
+        </Welcome> */}
 
-       {/* <Welcome name="Welcome component">
-        <p>Children props</p>
-       </Welcome> */}
+        {/* <Message /> */}
 
-       {/* <Message /> */}
+        {/* <Counter /> */}
 
-       {/* <Counter /> */}
+        {/* <FucntionClick />
 
-       {/* <FucntionClick />
+        <ClassClick /> */}
 
-       <ClassClick /> */}
+        {/* <EvenBind /> */}
 
-       {/* <EvenBind /> */}
+        {/* <ParentComponent /> */}
 
-       {/* <ParentComponent /> */}
+        {/* <UserGreeting /> */}
 
-       {/* <UserGreeting /> */}
+        {/* <NameList /> */}
 
-       {/* <NameList /> */}
-
-       {/* <Stylesheet primary='primary' /> */}
+        {/* <Stylesheet primary='primary' /> */}
 
       </div>
     );
