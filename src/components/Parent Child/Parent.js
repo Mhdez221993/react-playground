2edb2f1acc645ac4b9af1cadae2c68b213ae9c1c
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-28 00:34:34 +0100

    Optimize with useMemo and useCallback

--- src/components/Parent Child/Parent.js
+++ src/components/Parent Child/Parent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 
 import ChildTwo from './ChildTwo'
 
@@ -13,12 +13,18 @@ const Parent = ({children}) => {
 
   const handleClick = () => {}
 
+  const memoizedPerson = useMemo(() => person, [])
+  const memoizedHandleClick = useCallback(handleClick, [])
+
+
+
+
   console.log('Parent render')
   return (
     <div>
       <button onClick={() => setCount(prev => prev + 1)}>Parent Count = {count}</button>
       <button onClick={() => setName('John')}>Parent Count = {name}</button>
-      <ChildTwo handleClick={handleClick} name={name} />
+      <ChildTwo handleClick={memoizedHandleClick} name={name} />
     </div>
   )
 }
