5099739c7cd49819fee91f0ac8add46cd104e292
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-27 19:09:18 +0100

    Hook useRef part 1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import './App.css';
 import './components/appStyle.css';
 
-import React, { createContext } from 'react';
-
-import CounterThree from './components/CounterThree';
-
-export const CountContent  = createContext()
-
-
-const initicialState = 0
-const reducer = (state, action) => {
-  switch (action) {
-    case 'increment':
-      return state + 1
-    case 'decrement':
-      return state - 1
-    case 'reset':
-      return initicialState
-    default:
-      return state
-  }
-}
+import InputRef from './components/InputRef';
+import React from 'react';
 
 const App = () => {
-  // const [count, dispatch] = useReducer(reducer, initicialState)
-
     return (
       <div className="App">
-        <CounterThree/>
+        <InputRef />
       </div>
     );
 }
