fe276793fb78758c159e04cc4abd557c41845aef
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-30 15:45:20 +0100

    Learcn the whach mode of jest

--- react-testing/src/components/greet/greet.test.tsx
+++ react-testing/src/components/greet/greet.test.tsx
@@ -2,13 +2,13 @@ import { render, screen } from "@testing-library/react"
 
 import Greet from "./Greet"
 
-test('Greet component render correctly', () => {
+test('Greet render correctly', () => {
   render(<Greet />)
   const greet = screen.getByText('Hello')
   expect(greet).toBeInTheDocument()
 })
 
-test('Greet component render Hello Doe', () => {
+test('Greet renders a name', () => {
   render(<Greet name="Doe" />)
   const greet = screen.getByText('Hello Doe')
   expect(greet).toBeInTheDocument()
