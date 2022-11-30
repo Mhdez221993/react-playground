db0ce012f6ddd13d9e9b6f1fccb430605971d1ae
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-30 20:45:07 +0100

    Update Greet component

--- react-testing/src/components/greet/greet.test.tsx
+++ react-testing/src/components/greet/greet.test.tsx
@@ -9,9 +9,9 @@ describe('Greet', () => {
     expect(greet).toBeInTheDocument()
   })
 
-  // test('renders a name', () => {
-  //   render(<Greet name="Doe" />)
-  //   const greet = screen.getByText('Hello Doe')
-  //   expect(greet).toBeInTheDocument()
-  // })
+  test('renders a name', () => {
+    render(<Greet name="Doe" />)
+    const greet = screen.getByText('Hello Doe')
+    expect(greet).toBeInTheDocument()
+  })
 })
