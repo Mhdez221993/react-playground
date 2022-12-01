79b70c47e0a79f2238e6b2b1dfecf92bfd982a56
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-01 19:11:14 +0100

    Test decrement counter on useCounter hook

--- react-testing/src/hooks/use-counter/useCounter.test.tsx
+++ react-testing/src/hooks/use-counter/useCounter.test.tsx
@@ -1,4 +1,5 @@
-import { renderHook } from "@testing-library/react"
+import { act, renderHook } from "@testing-library/react"
+
 import useCounter from "./useCounter"
 
 describe('useCounter', () => {
@@ -15,5 +16,17 @@ describe('useCounter', () => {
     })
 
     expect(result.current.count).toBe(10)
+  })
+
+  test('shuld increment the counter', () => {
+    const {result} = renderHook(useCounter)
+    act(() => result.current.increment())
+    expect(result.current.count).toBe(1)
+  })
+
+  test('shuld decrement the counter', () => {
+    const {result} = renderHook(useCounter)
+    act(() => result.current.decrement())
+    expect(result.current.count).toBe(-1)
   })
 })
