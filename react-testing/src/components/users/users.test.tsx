c6669aeb121d4cec8b3a852c4f99af763c7eef91
Moises Hernandez <mhdezcoronado@gmail.com>
2022-12-01 21:22:00 +0100

    Handle error response with MSW

--- react-testing/src/components/users/users.test.tsx
+++ react-testing/src/components/users/users.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react"
 
 import Users from "./Users"
+import { rest } from "msw"
+import { server } from "../../mocks/server"
 
 describe('Users', () => {
   test('renders correctly', async () => {
@@ -14,5 +16,20 @@ describe('Users', () => {
     render(<Users />)
     const users = await screen.findAllByRole('listitem')
     expect(users).toHaveLength(3)
+  })
+
+  test('render error', async () => {
+    server.use(
+      rest.get(
+        'https://jsonplaceholder.typicode.com/users',
+        (req, res, ctx) => {
+          return res(ctx.status(500))
+        }
+      )
+    )
+
+    render(<Users />)
+    const error = await screen.findByText('Error fetching users')
+    expect(error).toBeInTheDocument()
   })
 })
