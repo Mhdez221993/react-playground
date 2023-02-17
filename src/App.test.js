d51a20f901ca7c4f93314afe9e29198e25242dc1
Moises Hernandez <mhdezcoronado@gmail.com>
2023-02-17 20:35:11 +0000

    .toBeInstanceOf(class)

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,29 @@
-function drinkEach(fn, arr) {
-  arr.forEach((beverage) => {
-    fn(beverage);
-  });
+// A class representing a Person
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
 }
 
-test('registration applies correctly to orange La Croix', () => {
-  const drink = jest.fn();
-  drinkEach(drink, ['lemon', 'octopus']);
-  expect(drink).toHaveBeenNthCalledWith(1, 'lemon');
-  expect(drink).toHaveBeenNthCalledWith(2, 'octopus');
+// A class representing a Student, which extends the Person class
+class Student extends Person {
+  constructor(name, age, grade) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  sayHello() {
+    return `Hello, my name is ${this.name}, I am ${this.age} years old, and I am in grade ${this.grade}.`;
+  }
+}
+
+// A Jest test that uses the `toBeInstanceOf` matcher
+test('a Student is an instance of Person', () => {
+  const student = new Student('John Doe', 16, 10);
+  expect(student).toBeInstanceOf(Person); // This checks that `student` is an instance of the `Person` class
 });
