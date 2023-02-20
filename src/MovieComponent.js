fbea98b56ed1aa0c69596e6a4e582e4719a3eeac
Moises Hernandez <mhdezcoronado@gmail.com>
2023-02-20 19:30:34 +0100

    Finish Search component

--- src/MovieComponent.js
+++ src/MovieComponent.js
@@ -1,23 +1,64 @@
+import { addMovie } from "./store";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+
+const initialState = { name: "", rating: "", duration: "" };
+
 const MovieComponent = () => {
+  const dispatch = useDispatch();
+  const [state, setState] = useState(initialState);
+
+  const handleState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    state.duration = Number(state.duration);
+    dispatch(addMovie(state));
+    setState(initialState);
+  };
+
   return (
     <div data-testid="movie-component" className="movies">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">Movie Name</label>
           <br />
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={state.name}
+            name="name"
+            onChange={handleState}
+          />
         </div>
 
         <div className="form-control">
           <label htmlFor="rating">Raiting</label>
           <br />
-          <input type="text" id="rating" />
+          <input
+            type="text"
+            id="rating"
+            value={state.rating}
+            name="rating"
+            onChange={handleState}
+          />
         </div>
 
         <div className="form-control">
           <label htmlFor="duration">Duration</label>
           <br />
-          <input type="text" id="duration" />
+          <input
+            type="text"
+            id="duration"
+            value={state.duration}
+            name="duration"
+            onChange={handleState}
+          />
         </div>
 
         <button className="add-movie" type="submit">
--- src/store.js
+++ src/store.js
@@ -4,8 +4,15 @@ const movieSlice = createSlice({
   name: "movie",
   initialState: { movies: [], search: [], noResult: "No movie found!" },
   reducers: {
-    addMovie: (state, action) => state.movies.push(action.payload),
-    searchMovie: (state, action) => state.search.push(action.payload),
+    addMovie(state, action) {
+      state.movies.push(action.payload);
+    },
+    searchMovie(state, action) {
+      const searchTerm = action.payload.toLowerCase();
+      state.search = state.movies.filter((movie) =>
+        movie.name.toLowerCase().includes(searchTerm)
+      );
+    },
   },
 });
 
