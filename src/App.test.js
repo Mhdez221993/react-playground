26f49c2d570c77ff1fe6f2469d59d3164f7f8953
Moises Hernandez <mhdezcoronado@gmail.com>
2023-02-20 22:59:53 +0100

    User before each to reduce code

--- src/App.test.js
+++ src/App.test.js
@@ -1,50 +1,42 @@
+/* eslint-disable testing-library/no-render-in-setup */
 import { fireEvent, render, screen } from "@testing-library/react";
 
 import App from "./App";
-import MovieComponent from "./MovieComponent";
 import { Provider } from "react-redux";
 import React from "react";
-import SearchComponent from "./SearchComponent";
 import store from "./store";
 
 describe("MovieComponent", () => {
-  it("render App component", () => {
-    render(
+  let app;
+  beforeEach(() => {
+    app = render(
       <Provider store={store}>
         <App />
       </Provider>
     );
+  });
 
+  afterEach(() => {
+    // Clean up after each test
+    app.unmount();
+  });
+
+  it("render App component", () => {
     const title = screen.getByRole("heading", { level: 1, name: "Movie App" });
     expect(title).toBeInTheDocument();
   });
 
   it("render MovieComponent", () => {
-    render(
-      <Provider store={store}>
-        <MovieComponent />
-      </Provider>
-    );
     const movie = screen.getByTestId("movie-component");
     expect(movie).toBeInTheDocument();
   });
 
   it("render the search component", () => {
-    render(
-      <Provider store={store}>
-        <SearchComponent />
-      </Provider>
-    );
     const search = screen.getByTestId("search-component");
     expect(search).toBeInTheDocument();
   });
 
   it("add a new movie", () => {
-    render(
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
     const nameInput = screen.getByTestId("name-input");
     const ratingInput = screen.getByTestId("rating-input");
     const durationInput = screen.getByTestId("duration-input");
