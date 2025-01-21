da33464826bec3aec8e40609d4cc1d7835316107
Moises Hernandez <mhdezcoronado@gmail.com>
2025-01-21 23:41:57 +0000

    Add counter functionality and update button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-const MyButton = () => <button>I'm a button</button>
+import { useState } from "react";
 
 function App() {
+    const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
-  <div>
-    <h1>Welcome to my App</h1>
-    <MyButton />
-  </div>)
+    <>
+      <h1>Counters Update separately</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </>
+  )
+}
+
+function MyButton({ count, onClick }) {
+  return (
+    <>
+      <button onClick={onClick}>Clicked {count} times</button>
+    </>
+  )
 }
 
 export default App;
